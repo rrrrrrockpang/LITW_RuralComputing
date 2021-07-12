1b7997bfc2c00b77564001a23ca613744b5a5705
rrrrrrockpang <ypang2@cs.washington.edu>
2021-07-12 12:25:08 -0700

    retrieve data from db

--- template/include/summary_data.php
+++ template/include/summary_data.php
@@ -3,12 +3,23 @@
 //IN: JSON - OUT:JSON
 function summary($json_data) {
 	$data = json_decode($json_data,true);
-	$cities = array();
-	foreach ($data as $value) {
-		$cities[$value['city']] = true;
+	// $cities = array();
+	// foreach ($data as $value) {
+	// 	$cities[$value['city']] = true;
+	// }
+	// $summary = array('city_number'=>sizeof($cities),'cities'=>array_keys($cities));
+	// return json_encode($summary);
+	$results = array(
+		'motivation_1_answer'=>array('sum'=>0, 'count'=>0)
+	);
+
+	foreach($data as $participant) {
+		$answer=intval($participant['motivation_1_answer']);
+		$results['motivation_1_answer']['sum'] += $answer;
+		$results['motivation_1_answer']['count']++;
 	}
-	$summary = array('city_number'=>sizeof($cities),'cities'=>array_keys($cities));
-	return json_encode($summary);
+
+	return json_encode($results);
 }
 
 //READ db_data.json
